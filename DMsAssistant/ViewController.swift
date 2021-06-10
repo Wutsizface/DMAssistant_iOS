dc2d882fd76bd8db70be5e0f35f9479c3a45d546
Jacob Russell <jacob_rssll@yahoo.com>
2021-06-10 08:46:03 -0600

    first commit
    adds everything

---
+++ DMsAssistant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DMsAssistant
+//
+//  Created by Jacob Russell on 6/1/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
